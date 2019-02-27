28c601fea3ae7a4445b731f2f84c3187b0a0c841
Brandon Turner <brandontheturner@gmail.com>
2019-02-27 15:24:28 -0600

    created functions and wrote tests

--- src/tests/calculations.test.js
+++ src/tests/calculations.test.js
@@ -1,7 +1,22 @@
-import {add, subtract, multiply,divide} from "../services/calculations";
+import { add, subtract, multiply, divide } from "../services/calculations";
 
 describe("add", () => {
   it("should add 1 and 2 and return 3", () => {
     expect(add(1, 2)).toBe(3);
+  });
+});
+describe("subtract", () => {
+  it("should subtract 1 and 2 and return 3", () => {
+    expect(subtract(5, 4)).toBe(1);
+  });
+});
+describe("multiply", () => {
+  it("should multiply 1 and 2 and return 3", () => {
+    expect(multiply(5, 4)).toBe(20);
+  });
+});
+describe("divide", () => {
+  fit("should divide 1 and 2 and return 3", () => {
+    expect(divide(100, 4)).toBe(25);
   });
 });
